cc87556cdfc42c91b65992a7efbf7f8af298a0f8
IGotYB <igotyb.stream@gmail.com>
2023-01-09 12:30:37 +0100

    initialization of origin project

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
