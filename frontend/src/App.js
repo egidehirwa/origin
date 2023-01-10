40bfa8abe42ac7fdd93c9b00993d2d6534bebf34
Munise Ben <munise0704@gmail.com>
2023-01-10 19:13:06 +0100

    change checked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import EventList from "./components/EventList";
 import TreeCard from "./components/TreeCard";
 import "./App.css";
-//munis = i check if the fonts & colors are working
+
+//munis (line 10-13) = i check if the fonts & colors are working
 function App() {
     return (
-        <div>
-            <EventList />
-            <TreeCard />
+        <div className="App">
+            <header className="App-header"></header>
+            <div>
+                <EventList />
+                <TreeCard />
+            </div>
+            <main></main>
+            <footer></footer>
         </div>
     );
 }
