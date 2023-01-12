5238c8eb6dc60bd0f9cfc6b0ae5dc02600f2a104
igotyb <igotyb.stream@gmail.com>
2023-01-12 11:04:03 +0100

    clear file & organise components file & folder together

--- frontend/src/components/TreeCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./TreeCard.css";
-
-function TreeCard() {
-    return <div className="tree-card text-xl font-primaryFont text-creemish">Your Tree</div>;
-}
-
-export default TreeCard;
