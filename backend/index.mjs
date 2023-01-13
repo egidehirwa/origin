227536385880e98596c134a542398d3e5fcaf732
audricdet <audric.det@hotmail.com>
2023-01-13 16:48:03 +0100

    Nearly finish fb auth

---
+++ backend/index.mjs
@@ -0,0 +1,115 @@
+/* // Facebook authentication 
+import express from 'express'
+import passport from 'passport'
+import { Strategy as FacebookStrategy } from 'passport-facebook';
+import session from 'express-session'
+import * as dotenv from 'dotenv'
+import client from './src/db/connect.mjs'
+const app = express() 
+dotenv.config()
+
+
+passport.use(new FacebookStrategy({
+    clientID : '1377880669709451',
+    clientSecret : '066e4e587ee668e1425804619dbb2d0d', 
+    callbackURL : 'http://localhost:3000/auth/facebook/callback'
+},
+
+    async function(accessToken, refreshToken, profile, cb) {
+        // let user_profile = SELECT * FROM profiles WHERE facebook_id === profile.id
+        // if user_profile.exists?
+        // res.profile = user_profile
+        // if user_profile.not.exists?
+        // redirect user to create profile page
+        // res.facebook_id = profile.id
+        let user_profile  = await client.query(`SELECT * FROM profile WHERE facebook_id = ${profile.id}`)
+            if (user_profile.length > 0) {
+                res.profile = user_profile[0];
+            } else {
+                res.redirect('/create-profile');
+                res.facebook_id = profile.id;
+}
+        console.log(profile)
+        cb(null, {})
+    }
+))
+
+app.use(passport.initialize())
+
+app.get('/auth/facebook', passport.authenticate('facebook'))
+
+app.get('/auth/facebook/callback', 
+passport.authenticate('facebook', { session : false }),
+    function(req,res) {
+    console.log(res, req)
+    //successful authentication, redirect home.
+    res.send('AUTH WAS GOOD')
+})
+
+
+app.listen(3000, () => {
+    console.log('Origin app listening on port 3000!');
+}); */
+
+
+import express from 'express'
+import passport from 'passport'
+import { Strategy as FacebookStrategy } from 'passport-facebook';
+import session from 'express-session'
+import * as dotenv from 'dotenv'
+import client from './src/db/connect.mjs'
+const app = express() 
+dotenv.config()
+
+passport.use(new FacebookStrategy({
+    clientID : '1377880669709451',
+    clientSecret : '066e4e587ee668e1425804619dbb2d0d', 
+    callbackURL : 'http://localhost:3000/auth/facebook/callback'
+},
+async function(accessToken, refreshToken, profile, done) {
+
+    try {
+        let user_profile  = await client.query(`SELECT * FROM profile WHERE facebook_id = ${profile.id}`)
+        console.log(profile.id)
+        console.log(user_profile)
+        console.log(user_profile.length)
+        if (user_profile.length > 0) {
+            return done(null, {redirect: `/profile/${user_profile[0].id}`})
+        } else {
+            return done(null, {redirect: '/create-profile'})
+        }
+    } catch (error) {
+        console.log(error);
+        return done(error);
+    }
+}
+))
+
+app.use(passport.initialize())
+
+app.get('/auth/facebook', passport.authenticate('facebook'))
+
+app.get('/auth/facebook/callback', 
+passport.authenticate('facebook', { session : false }),
+    function(req,res) {
+    if(req.user.redirect){
+        return res.redirect(req.user.redirect);
+    }
+    //successful authentication, redirect home.
+    res.redirect('/');
+})
+
+/*app.get('/auth/facebook/callback', 
+passport.authenticate('facebook', { session : false }),
+    function(req,res) {
+    if(req.user.redirect){
+        return res.redirect(req.user.redirect);
+    }
+    //successful authentication, redirect home.
+    res.redirect('/');
+})*/
+
+app.listen(3000, () => {
+    console.log('Origin app listening on port 3000!');
+});
+
--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE "user"(
+CREATE TABLE "users"(
     "id" BIGINT NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
-    "user" ADD PRIMARY KEY("id");
+    "users" ADD PRIMARY KEY("id");
 CREATE TABLE "profile"(
     "id" BIGINT NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "lastname" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255),
+    "lastname" VARCHAR(255),
     "dateOfBirth" DATE,
     "placeOfBirth" VARCHAR(255),
     "school" VARCHAR(255),
@@ -18,7 +18,10 @@ CREATE TABLE "profile"(
     "bio" VARCHAR(255),
     "user_id" BIGINT,
     "profilePicture" VARCHAR(255),
-    "private" BOOLEAN NOT NULL
+    "private" BOOLEAN,
+    "facebook_id" BIGINT,
+    "google_id" BIGINT,
+    "apple_id" BIGINT 
 );
 ALTER TABLE
     "profile" ADD PRIMARY KEY("id");
@@ -46,6 +49,6 @@ ALTER TABLE
 ALTER TABLE
     "event" ADD CONSTRAINT "event_profile_id_foreign" FOREIGN KEY("profile_id") REFERENCES "profile"("id");
 ALTER TABLE
-    "profile" ADD CONSTRAINT "profile_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "user"("id");
+    "profile" ADD CONSTRAINT "profile_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id");
 ALTER TABLE
     "user_per_family" ADD CONSTRAINT "user_per_family_family_id_foreign" FOREIGN KEY("family_id") REFERENCES "family"("id");
