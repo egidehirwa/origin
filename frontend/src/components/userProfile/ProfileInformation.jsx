2b4536f75e3e9342538af0465228aef17c76facd
Munise Ben <munise0704@gmail.com>
2023-01-14 13:06:05 +0100

    ProfileInformation function added

--- frontend/src/components/userProfile/ProfileInformation.jsx
+++ frontend/src/components/userProfile/ProfileInformation.jsx
@@ -1,65 +1,60 @@
 import React from "react";
 
-function ProfileInformation({ title, imageUrl, body }) {
-    if (false) {
-        // TODO: get data from database
+function isDataAvailable() {
+    //TODO: check if data available in database
+    return true;
+}
+
+function getElement(title, iconUrl, info) {
+    return (
+        <div>
+            <div className="flex flex-row">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 25" stroke-width="1.5" stroke="#fffbeb" class="w-8 h-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d={iconUrl} />
+                </svg>
+                <div className="p-2 font-secondaryFont   text-lighcream">{title}</div>
+            </div>
+            <div className="pl-10 font-secondaryFont text-creemish">{info}</div>
+        </div>
+    );
+}
+
+function getInformation() {
+    if (isDataAvailable()) {
+        //TODO: get data from database
         return (
             <div>
-                <div className=" px-6 py-4">
-                    <div className="py-4 font-primaryFont text-xl text-lighcream">Informations</div>
-
-                    <p className="p-2 font-secondaryFont outline rounded-md outline-offset-2 outline-creemish  text-white">No information available.</p>
-                </div>
+                {getElement(
+                    "Professional Experience",
+                    "M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z",
+                    "Further info can be added here1"
+                )}
+                {getElement(
+                    "Education",
+                    "M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5",
+                    "Further info can be added here2"
+                )}
+                {getElement(
+                    "Hobbies",
+                    "M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z",
+                    "Further info can be added here3"
+                )}
             </div>
         );
     } else {
-        return (
-            <div>
-                <div className=" px-6 py-4">
-                    <div className="py-4 font-primaryFont text-xl text-lighcream">Informations</div>
-
-                    <div className="flex flex-row">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 25" stroke-width="1.5" stroke="#fffbeb" class="w-8 h-8">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
-                            />
-                        </svg>
-
-                        <div className="p-2 font-secondaryFont   text-lighcream">Professional Experience </div>
-                    </div>
-                    <div className="pl-10 font-secondaryFont text-creemish">info can be added (later)</div>
-
-                    <div className="flex flex-row">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 25" stroke-width="1.5" stroke="#fffbeb" class="w-8 h-8">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
-                            />
-                        </svg>
-
-                        <p className="p-2 font-secondaryFont   text-lighcream">Education </p>
-                    </div>
-                    <div className="pl-10 font-secondaryFont text-creemish">info can be added (later)</div>
-
-                    <div className="flex flex-row">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 25" stroke-width="1.5" stroke="#fffbeb" class="w-8 h-8">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z"
-                            />
-                        </svg>
-
-                        <p className="p-2 font-secondaryFont   text-lighcream">Hobbies </p>
-                    </div>
-                    <div className="pl-10 font-secondaryFont text-creemish">info can be added (later)</div>
-                </div>
-            </div>
-        );
+        return <p className="p-2 font-secondaryFont outline rounded-md outline-offset-2 outline-creemish  text-white">No information available.</p>;
     }
+}
+
+function ProfileInformation({ title, iconUrl, info }) {
+    return (
+        <div>
+            <div className=" px-6 py-4">
+                <div className="py-4 font-primaryFont text-xl text-lighcream">Informations</div>
+                {getInformation()}
+            </div>
+        </div>
+    );
 }
 
 export default ProfileInformation;
