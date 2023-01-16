37c1513a7784cdf02f9bf0796d9fea890a470287
Daria <spodina.darya@gmail.com>
2023-01-16 16:56:47 +0100

    updates

--- frontend/src/components/header/desktopHeader.jsx
+++ frontend/src/components/header/desktopHeader.jsx
@@ -19,21 +19,3 @@ export default function DesktopHeaderLogIn () {
         </div>
     )
 }
-
-export function DesktopHeaderLogOut () {
-    return (
-        <div className="flex flex-row  ">
-            <img className="iteams-left" src="./origin_logo_cream.png" alt="origin_logo" />
-            <div className="bg-greenish items-center text-lightcream/24">
-                <a className="text-lightcream/24" href="#">About Us</a>
-                <a className="text-lightcream/24" href="#">Tree</a>
-                <a className="text-lightcream/24" href="#">Profile</a>
-            </div>
-            <LogOutButton />
-            <div className="flex iteams-left">
-                <ShoppingCartIcon className="h-6 w-6" />
-                <p>Shop</p>
-            </div>
-        </div>
-    )
-}
