95768f82ca5d44ddd45a78c899c1d532b90d0883
IGotYB <igotyb.stream@gmail.com>
2023-01-19 11:30:12 +0100

    new clean master branch, fresh for new features and happiness

--- frontend/src/layouts/rootLayout.jsx
+++ frontend/src/layouts/rootLayout.jsx
@@ -1,20 +1,18 @@
 
 import { Outlet } from "react-router-dom";
 import FooterMobile from "../components/footer/footerMobile";
-
-
-import FooterMobile from "../components/footer/footerMobile";
 import FooterDesktop from "../components/footer/footerDesktop";
 
 const RootLayout = () => {
   return (
-    <div className="root-layout flex flex-col h-screen bg-greenish">
+    <div className="root-layout flex flex-col min-h-screen bg-greenish">
       <header className="h-10"></header>
       <main className="container mx-auto mb-auto">
         < Outlet />
       </main>
       <footer className="h-15">
         < FooterMobile />
+        < FooterDesktop />
       </footer>
     </div>
   );
