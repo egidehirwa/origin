d357a44cf5e35485edc8cbb8da2fdeb153d57c55
audricdet <audric.det@hotmail.com>
2023-01-19 15:26:45 +0100

    ALL AUTH WORKING

--- backend/src/api/auth/googleAuth.mjs
+++ backend/src/api/auth/googleAuth.mjs
@@ -8,79 +8,62 @@ import client from '../../db/connect.mjs'
 import express from 'express'
 const googleRouter = express.Router()
 dotenv.config()
-const app = express()
 
-app.use(session({
-    secret: 'your_secret_key',
-    resave: false,
-    saveUninitialized: true,
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+const APIKey = 'AIzaSyAUd9H-t8MSHzAAUu7Jfy0MBjynSf4KM8U'
 
 
 passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/g/google/callback",
-    },
-    function (accessToken, refreshToken, profile, done) {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/g/google/callback",
+    passReqToCallback: true
+},
 
-        // try {
-        //     console.log(profile)
-        //     let user_profile = await client.query(`SELECT * FROM profile WHERE google_id = '${profile.id}'`)
-        //     console.log(profile.id)
-        //     console.log(user_profile)
-        //     if (user_profile.rows.length > 0) {
-        //         return done(null, {
-        //             redirect: `/profile/${user_profile.rows[0].id}`
-        //         })
-        //     } else {
-        //         return done(null, {
-        //             redirect: '/create-profile'
-        //         })
-        //     }
-        // } catch (error) {
-        //     console.log(error);
-        //     return done(error);
-        // }
+    async function (request, accessToken, refreshToken, profile, done) {
+        try {
+            // Check if user has logged in before using Google
+            let user_profile = await client.query(`SELECT * FROM profile WHERE google_id = '${profile.id}'`)
+            if (user_profile.rows.length > 0) {
+                // User has logged in before, redirect to profile page
+                return done(null, {redirect: `/profile/${user_profile.rows[0].id}`})
+            } else {
+                // User is logging in for the first time, redirect to create-profile page
+                return done(null, {redirect: '/create-profile'})
+            }
+        } catch (error) {
+            console.log(error);
+            return done(error);
+        }
+        
     }
-    
 ));
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done){
     done(null, user);
 })
 
-passport.deserializeUser(function (user, done) {
+passport.deserializeUser(function(user, done){
     done(null, user);
 })
 
-googleRouter.get('/google', (req, res) => {
+googleRouter.get('/google', (req,res) => {
     res.send('<a href="/auth/g/auth/google">Authenticate with Google</a>')
 })
 
 
 googleRouter.get('/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile']
-    })
+    passport.authenticate('google',{ scope: ['email', 'profile']})
 )
 
-googleRouter.get('/google/callback', (req, res) => {
-    passport.authenticate('google', {
-                successRedirect: '/protected',
-                failureRedirect: '/auth/failure',
-            })
+googleRouter.get('/google/callback',passport.authenticate('google', {session: false}), (req, res) => {
+    console.log(req.user)
+    // if (req.user.redirect) {
+        // redirect user to the specified page
+        res.redirect(req.user.redirect);
+    // } else {
+        // handle error or redirect to default page
+    // }
 });
 
-googleRouter.get('/failure', (req, res) => {
-    res.send('We have a problem')
-})
-
-
-googleRouter.get('/protected', (req, res) => {
-    res.send(`YEAH YEAH YEAH ${req.user.displayName}`)
-})
 
 export default googleRouter
