441f76cce2eb729ee6e4c5c7f2ffb1b91a17bc93
igotyb <igotyb.stream@gmail.com>
2023-01-31 21:38:10 +0100

    set header with z-index so it goes up the images in about page

--- frontend/src/layouts/rootLayout.jsx
+++ frontend/src/layouts/rootLayout.jsx
@@ -6,14 +6,14 @@ import DesktopHeader from "../components/header/desktopHeader";
 const RootLayout = ({ logedIn }) => {
   return (
     <div className="root-layout min-h-screen grid grid-rows-[auto_1fr_auto] bg-greenish text-lightcream">
-      <header className="lg:mb-10">
+      <header className="lg:mb-10 z-10">
         <DesktopHeader logedIn={logedIn} />
       </header>
       <main className="">
         < Outlet />
       </main>
-      <footer >
-        < FooterMobile />
+      <footer className="z-10">
+        < FooterMobile />s
         < FooterDesktop />
       </footer>
     </div>
