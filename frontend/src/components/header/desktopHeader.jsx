47fbb7627db66c90c8ff65cc2fb12077730c3438
igotyb <igotyb.stream@gmail.com>
2023-01-31 14:16:51 +0100

    changed for attribut to htmlFor and class to className in contact us page & add container on header

--- frontend/src/components/header/desktopHeader.jsx
+++ frontend/src/components/header/desktopHeader.jsx
@@ -9,7 +9,8 @@ const DesktopHeader = ({ logedIn }) => {
 
 const DesktopHeaderLogIn = () => {
   return (
-      <div className="bg-lightgreen min-h-4 hidden lg:flex justify-around items-center text-lightcream">
+    <div className="bg-lightgreen text-lightcream">
+      <div className="container mx-auto min-h-4 hidden lg:flex justify-around items-center">
         <img src={logo} alt="Origin Logo" className="max-h-24" />
         <div className="bg-darkgreen w-2/4 min-h-[50px] rounded-xl flex items-center text-center font-semibold">
           <Link to="/" className="w-1/3">Home</Link>
@@ -23,25 +24,26 @@ const DesktopHeaderLogIn = () => {
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
 const DesktopHeaderLogOut = () => {
   return (
     <div className="bg-lightgreen min-h-4 hidden lg:flex justify-around items-center text-lightcream">
-        <img src={logo} alt="Origin Logo" className="max-h-24" />
-        <div className="bg-darkgreen w-2/4 min-h-[50px] rounded-xl flex items-center text-center font-semibold">
-          <Link to="/about" className="w-1/3">About us</Link>
-          <Link to="/tree" className="w-1/3">Tree</Link>
-          <Link to="/profile" className="w-1/3">Profile</Link>
-        </div>
-        <div className="flex space-x-2">
-          <LogOutButton />
-          <div className="flex">
-            <Link to="/shop"><img src={shoppingcart} alt=""></img></Link>
-          </div>
+      <img src={logo} alt="Origin Logo" className="max-h-24" />
+      <div className="bg-darkgreen w-2/4 min-h-[50px] rounded-xl flex items-center text-center font-semibold">
+        <Link to="/about" className="w-1/3">About us</Link>
+        <Link to="/tree" className="w-1/3">Tree</Link>
+        <Link to="/profile" className="w-1/3">Profile</Link>
+      </div>
+      <div className="flex space-x-2">
+        <LogOutButton />
+        <div className="flex">
+          <Link to="/shop"><img src={shoppingcart} alt=""></img></Link>
         </div>
       </div>
+    </div>
   );
 };
 
--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -3,72 +3,72 @@ import contactImg from "../assets/background/contact.png"
 const ContactPage = () => {
     return (
         <div>
-            <section class="p-8 container mx-auto relative bg-gradient-to-r from-[#7b8b7f] to-[#7b8b7f] " id="contact_page">
-                <div class=" grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-12 p-8 ">
-                    <div class="lg:col-span-7">
-                        <h6 class="p-2 font-primaryFont text-xxl text-lightcream">Contact Us</h6>
+            <section className="p-8 container mx-auto relative bg-gradient-to-r from-[#7b8b7f] to-[#7b8b7f] " id="contact_page">
+                <div className=" grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-12 p-8 ">
+                    <div className="lg:col-span-7">
+                        <h6 className="p-2 font-primaryFont text-xxl text-lightcream">Contact Us</h6>
 
-                        <form action="#" class="space-y-8">
+                        <form action="#" className="space-y-8">
                             <div>
-                                <label for="name" class="block mb-2 text-sm font-secondaryFont text-gray-900 ">
+                                <label htmlFor="name" className="block mb-2 text-sm font-secondaryFont text-gray-900 ">
                                     Your name
                                 </label>
                                 <input
                                     type="name"
                                     id="name"
-                                    class="shadow-sm text-gray-900 text-sm rounded-lg block w-full p-2.5 bg-lightcream "
+                                    className="shadow-sm text-gray-900 text-sm rounded-lg block w-full p-2.5 bg-lightcream "
                                     placeholder="name"
                                     required
                                 />
                             </div>
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-secondaryFont text-gray-900  ">
+                                <label htmlFor="email" className="block mb-2 text-sm font-secondaryFont text-gray-900  ">
                                     Your email adress
                                 </label>
                                 <input
                                     type="email"
                                     id="email"
-                                    class="shadow-sm text-gray-900 text-sm rounded-lg block w-full p-2.5 bg-lightcream "
+                                    className="shadow-sm text-gray-900 text-sm rounded-lg block w-full p-2.5 bg-lightcream "
                                     placeholder="mail@mail.com"
                                     required
                                 />
                             </div>
                             <div>
-                                <label for="subject" class="block mb-2 text-sm font-secondaryFont text-gray-900  ">
+                                <label htmlFor="subject" className="block mb-2 text-sm font-secondaryFont text-gray-900  ">
                                     Subject
                                 </label>
                                 <input
                                     type="text"
                                     id="subject"
-                                    class="shadow-sm text-gray-900 text-sm rounded-lg block w-full p-2.5 bg-lightcream "
+                                    className="shadow-sm text-gray-900 text-sm rounded-lg block w-full p-2.5 bg-lightcream "
                                     placeholder="Let us know how we can help you"
                                     required
                                 />
                             </div>
-                            <div class="sm:col-span-2">
-                                <label for="message" class="block mb-2 text-sm font-secondaryFont text-gray-900  ">
+                            <div className="sm:col-span-2">
+                                <label htmlFor="message" className="block mb-2 text-sm font-secondaryFont text-gray-900  ">
                                     Your message
                                 </label>
                                 <textarea
                                     id="message"
                                     rows="6"
-                                    class="shadow-sm text-gray-900 text-sm rounded-lg block w-full p-2.5 bg-lightcream "
+                                    className="shadow-sm text-gray-900 text-sm rounded-lg block w-full p-2.5 bg-lightcream "
                                     placeholder="Leave a comment..."
                                 ></textarea>
                             </div>
-                            <div class="relative mt-10">
-                                <a href="https://www.google.be/" class="btn rounded-md px-4 py-4  bg-greenish text-white hover:bg-creemish">
+                            <div className="relative mt-10">
+                                <a href="https://www.google.be/" className="btn rounded-md px-4 py-4  bg-greenish text-white hover:bg-creemish">
                                     Send
                                 </a>
                             </div>
                         </form>
                     </div>
-                    <div class="lg:col-span-5 relative mt-16">
-                        <img src={contactImg} class="" alt="" />
+                    <div className="lg:col-span-5 relative mt-16">
+                        <img src={contactImg} className="" alt="" />
                     </div>
                 </div>
             </section>
-            <section class="px-10 py-10 relative bg-greenish" id="style"></section>
+            <section className="px-10 py-10 relative bg-greenish" id="style"></section>
         </div>
     );
 };
