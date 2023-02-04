9928d7bc81ad2baa896962a28fe499cef0bb7746
Daria <spodina.darya@gmail.com>
2023-02-04 15:50:46 +0100

    Renewed lost data after merge

--- frontend/src/components/buttons/logButton.jsx
+++ frontend/src/components/buttons/logButton.jsx
@@ -13,19 +13,16 @@ import { Link } from "react-router-dom";
 
 const LogInButton = () => {
     return (
-        <button className="border-gradient gradient-red min-w-[80px] max-h-[50px] px-2">
-            <Link to="/connexion">
-                LogIn
-            </Link>
-        </button>
+            <button className="bg-gradient-red lg px-12 min-h-[50px] text-darkgreen items-center">
+           <Link to="/profile">LogIn</Link>
+            </button>
     )
 }
 const LogOutButton = () => {
     return (
-        <div>
-            <Link to="/" className="border-gradient gradient-red px-4">LogOut</Link>
-            {/* need to add a logic to actually disconnect the person from the plateform */}
-        </div>
+        <button className="bg-gradient-red lg px-12 min-h-[50px] text-darkgreen items-center">
+        <Link to="/">LogOut</Link>
+         </button>
     )
 }
 
