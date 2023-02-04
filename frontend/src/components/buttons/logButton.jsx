d1bc552f949b3d2d6d455b5e141d578eb0ff2ee4
igotyb <igotyb.stream@gmail.com>
2023-02-04 21:50:18 +0100

    logButton redirect to connexion

--- frontend/src/components/buttons/logButton.jsx
+++ frontend/src/components/buttons/logButton.jsx
@@ -13,16 +13,16 @@ import { Link } from "react-router-dom";
 
 const LogInButton = () => {
     return (
-            <button className="bg-gradient-red lg px-12 min-h-[50px] text-darkgreen items-center">
-           <Link to="/profile">LogIn</Link>
-            </button>
+        <button className="bg-gradient-red lg px-12 min-h-[50px] text-darkgreen items-center">
+            <Link to="/connexion">LogIn</Link>
+        </button>
     )
 }
 const LogOutButton = () => {
     return (
         <button className="bg-gradient-red lg px-12 min-h-[50px] text-darkgreen items-center">
-        <Link to="/">LogOut</Link>
-         </button>
+            <Link to="/">LogOut</Link>
+        </button>
     )
 }
 
